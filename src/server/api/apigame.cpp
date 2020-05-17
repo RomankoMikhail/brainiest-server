008ebe5ab20eb483dc1b7eead2ad039d48c8919e
RomankoMikhail <romanko.mikhail@yandex.ru>
2020-05-17 09:19:14 +0300

    Add missing files

--- src/server/api/apigame.cpp
+++ src/server/api/apigame.cpp
@@ -6,3 +6,23 @@
 #include "token.h"
 
 #include <QJsonArray>
+
+void onGameAdd(const HttpRequest &request, HttpResponse &response)
+{
+
+}
+
+void onGameList(const HttpRequest &request, HttpResponse &response)
+{
+
+}
+
+void onGameListOpen(const HttpRequest &request, HttpResponse &response)
+{
+
+}
+
+void onGameListComplete(const HttpRequest &request, HttpResponse &response)
+{
+
+}
